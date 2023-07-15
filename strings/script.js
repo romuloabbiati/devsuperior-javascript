a4db0e2bcd2cfc4d1876b208611b4189a8d1713c
romuloabbiati <rhabbiati@hotmail.com>
2023-07-15 12:10:16 +0100

    strings part 2

--- strings/script.js
+++ strings/script.js
@@ -1,3 +1,4 @@
+/*
 console.log("A literal string can be used within a double quotes");
 console.log('A literal string can be used within a single quotes');
 console.log(`A literal string can be used within a backticks`);
@@ -30,3 +31,36 @@ console.log(`Type of ${num4} is ${typeof num4}`);
 
 const num5 = parseFloat("14.5", 10);
 console.log(`Type of ${num5} is ${typeof num5}`);
+*/
+
+const str = "Maria Silva  ";
+
+console.log(`"const str = "Maria Silva  "`);
+
+console.log(`toLowerCase: ${str.toLowerCase()}`);
+
+console.log(`toUpperCase: ${str.toUpperCase()}`);
+
+console.log(`charAt: ${str.charAt(3)}`);
+console.log(`charAt: ${str[3]}`);
+
+console.log(`replace: ${str.replace("i", "$")}`);
+console.log(`replace: ${str.replace(/i/g, "$")}`);
+
+console.log(`length: ${str.length}`);
+
+console.log(`indexOf: ${str.indexOf("i")}`);
+
+console.log(`lastIndexOf: ${str.lastIndexOf("i")}`);
+
+console.log(`substring: ${str.substring(3, 8)}`);
+
+const data = "15/07/2023";
+const mes = data.substring(3, 5);
+console.log(mes);
+
+const value = 200.99;
+const newValue = value.toString().replace('.', ',');
+console.log(newValue);
+
+console.log(`trim: ${str.trim()}`);
