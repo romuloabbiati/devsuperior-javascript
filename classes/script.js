7fe15e7f5414423f4de30a2c2d092e9a35e729e1
romuloabbiati <rhabbiati@hotmail.com>
2023-07-20 09:02:22 +0100

    classes in JS

---
+++ classes/script.js
@@ -0,0 +1,30 @@
+class Product {
+
+    constructor(name, price, quantity) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    total() {
+        return this.price * this.quantity;
+    }
+
+    add(amount) {
+        this.quantity += amount;
+    }
+
+    remove(amount) {
+        if(this.quantity >= amount) {
+            this.quantity -= amount;
+        }
+    }
+
+    label() {
+        return "Data: " + this.name + ", " + this.price.toFixed(2);
+    }
+}
+
+const p1 = new Product("Monitor", 800.00, 10);
+
+const p2 = new Product("Mouse", 50.00, 4);
