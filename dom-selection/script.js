cc8b30b33e6cc0fa7c365e64cfe827bc162494fa
romuloabbiati <rhabbiati@hotmail.com>
2023-07-24 22:07:40 +0100

    dom

---
+++ dom-selection/script.js
@@ -0,0 +1,9 @@
+const item1 = document.querySelector('li');
+
+console.log(item1);
+console.log(item1.innerHTML);
+console.log(item1.outerHTML);
+
+const items = document.querySelectorAll('li');
+
+console.log(items);
